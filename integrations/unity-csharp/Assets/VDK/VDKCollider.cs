27e42b2e00d33620d02cec150cb8b35fa7abe9a5
Braden Wockner <bwockner@euclideon.com>
2020-02-13 14:07:10 +1000

    Added Vehicle examples and simplified collider

--- integrations/unity-csharp/Assets/VDK/VDKCollider.cs
+++ integrations/unity-csharp/Assets/VDK/VDKCollider.cs
@@ -259,26 +259,21 @@ public class VDKCollider : MonoBehaviour
         if (followTarget != null)
         {
             if (lockRotationToBody)
-                offset = Matrix4x4.Rotate(followTarget.transform.rotation) * new Vector4(watcherPos.x, watcherPos.y, watcherPos.z);
-            else
-                offset = Matrix4x4.Rotate(transform.rotation) * new Vector4(watcherPos.x, watcherPos.y, watcherPos.z);
+                this.transform.rotation = followTarget.transform.rotation;
 
+            offset = Matrix4x4.Rotate(transform.rotation) * new Vector4(watcherPos.x, watcherPos.y, watcherPos.z);
             if (threshholdFollow)
             {
                 if ((this.transform.position - followTarget.transform.position).magnitude > followThreshold)
                 {
                     this.transform.position = followTarget.transform.position + offset;
                     UpdateView();
-                    if(lockRotationToBody)
-                      this.transform.rotation = followTarget.transform.rotation;
                 }
             }
             else
             {
-                this.transform.position = followTarget.transform.position + offset;//+followTarget.transform.TransformVector(watcherPos);
+                this.transform.position = followTarget.transform.position + offset;
                 UpdateView();
-                if(lockRotationToBody)
-                  this.transform.rotation = followTarget.transform.rotation;
             }
         }
         else
